dd0ab17ff8748a091a602d260f3c443c06c5a5e4
miguelaarrones <miguelaarrones@icloud.com>
2023-12-18 19:21:27 +0100

    Folder reorganization and added a resource manager

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
+
+    private void Awake()
+    {
+        resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
+        ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            resourceAmountDictionary[resourceType] = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+            AddResource(resourceTypeList.list[0], 2);
+            TestLogResourceAmountDictionary();
+        }
+    }
+
+    public void TestLogResourceAmountDictionary()
+    {
+        foreach (ResourceTypeSO resourceType in resourceAmountDictionary.Keys)
+        {
+            Debug.Log($"{resourceType.nameString}: {resourceAmountDictionary[resourceType]}");
+        }
+    }
+
+    public void AddResource(ResourceTypeSO resourceType, int amount)
+    {
+        resourceAmountDictionary[resourceType] += amount;
+    }
+}

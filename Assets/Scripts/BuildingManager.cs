abccaea4c4fd32e34b5b9fbb5489398fbbd544ee
miguelaarrones <miguelaarrones@icloud.com>
2023-12-18 17:58:58 +0100

    Added a new building type and a way to manage them

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
-    [SerializeField] private Transform woodHarvester;
+    private BuildingTypeListSO buildingTypeList;
+    private BuildingTypeSO buildingType;
+
+    private void Start()
+    {
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        buildingType = buildingTypeList.list[0];
+    }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(woodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            buildingType = buildingTypeList.list[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            buildingType = buildingTypeList.list[1];
         }
     }
 
